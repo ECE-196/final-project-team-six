672ba76caf0bcd53455f690b1eaf9c2544c30f94
PurabB <117234132+PurabB@users.noreply.github.com>
2024-12-02 16:59:25 -0800

    Update template_IKControl.ino

--- template_IKControl.ino
+++ template_IKControl.ino
@@ -12,6 +12,10 @@
 #define L1 304.8           // Length of the first link
 #define L2 304.8           // Length of the second link
 
+// Target coordinates (initialize to home position)
+double targetX = 200.0;
+double targetY = 200.0;
+
 void setup() {
   // Initialize motor pins as outputs
   pinMode(STEP_PIN_1, OUTPUT);
@@ -21,18 +25,58 @@ void setup() {
 
   // Initialize serial communication
   Serial.begin(9600);
-  Serial.println("Enter target X and Y coordinates:");
+  Serial.println("Use W (up), S (down), A (left), D (right) to move the arm.");
 }
 
 void loop() {
   if (Serial.available() > 0) {
-    // Read input coordinates
-    String input = Serial.readStringUntil('\n');
-    double x, y;
-    sscanf(input.c_str(), "%lf %lf", &x, &y);
+    char command = Serial.read();  // Read the command input
 
-    // Move the robotic arm to the specified position
-    moveToPosition(x, y);
+    // Adjust target coordinates based on command
+    switch (command) {
+      case 'W':  // Move up
+        targetY += 10;
+        break;
+      case 'S':  // Move down
+        targetY -= 10;
+        break;
+      case 'A':  // Move left
+        targetX -= 10;
+        break;
+      case 'D':  // Move right
+        targetX += 10;
+        break;
+      default:
+        Serial.println("Invalid command. Use W, S, A, D.");
+        return;
+    }
+
+    // Calculate new angles based on target coordinates
+    double theta1, theta2;
+    if (calculateAngles(targetX, targetY, &theta1, &theta2)) {
+      // Convert angles from radians to degrees
+      theta1 = theta1 * 180 / M_PI;
+      theta2 = theta2 * 180 / M_PI;
+
+      Serial.print("Moving to position: X = ");
+      Serial.print(targetX);
+      Serial.print(", Y = ");
+      Serial.println(targetY);
+      Serial.print("Target angles: Theta1 = ");
+      Serial.print(theta1);
+      Serial.print(", Theta2 = ");
+      Serial.println(theta2);
+
+      // Calculate the number of steps for each motor
+      int steps1 = (int)(theta1 / STEP_ANGLE);
+      int steps2 = (int)(theta2 / STEP_ANGLE);
+
+      // Move stepper motors to the target position
+      moveStepper(STEP_PIN_1, DIR_PIN_1, abs(steps1), steps1 > 0);
+      moveStepper(STEP_PIN_2, DIR_PIN_2, abs(steps2), steps2 > 0);
+    } else {
+      Serial.println("Target position is unreachable.");
+    }
   }
 }
 
@@ -60,30 +104,4 @@ bool calculateAngles(double x, double y, double* theta1, double* theta2) {
 
   *theta1 = atan2(y, x) - atan2(L2 * sinTheta2, L1 + L2 * cosTheta2);
   return true;  // Success
-}
-
-void moveToPosition(double x, double y) {
-  double theta1, theta2;
-
-  if (!calculateAngles(x, y, &theta1, &theta2)) {
-    Serial.println("Position is unreachable.");
-    return;
-  }
-
-  // Convert angles from radians to degrees
-  theta1 = theta1 * 180 / M_PI;
-  theta2 = theta2 * 180 / M_PI;
-
-  Serial.print("Target angles: Theta1 = ");
-  Serial.print(theta1);
-  Serial.print(", Theta2 = ");
-  Serial.println(theta2);
-
-  // Calculate the number of steps required for each motor
-  int steps1 = (int)(theta1 / STEP_ANGLE);
-  int steps2 = (int)(theta2 / STEP_ANGLE);
-
-  // Move stepper motors to the target positions
-  moveStepper(STEP_PIN_1, DIR_PIN_1, abs(steps1), steps1 > 0);
-  moveStepper(STEP_PIN_2, DIR_PIN_2, abs(steps2), steps2 > 0);
 }
